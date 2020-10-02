ed466c1ac85e523821a2470671844e3e6d21efa7
ewarren1994 <37820139+steelcity412@users.noreply.github.com>
2020-10-02 19:05:59 -0400

    Added Bubble Sort algorithm
    Bubble Sort is another O(N^2) algorithm

--- BigONotationSample/Program.cs
+++ BigONotationSample/Program.cs
@@ -6,18 +6,31 @@ namespace BigONotationSample
     class Program
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("Hello World!");
-
-            Console.WriteLine("1. O(1) algorithm: " + isMyNameEvan("Evan"));
-
+        {
             string[] arr = { "John", "Jane", "Evan" };
             string[] arr2 = { "Evan", "Peter", "Eric" };
 
-            Console.WriteLine("2.) O(N) algorithm: " + isMyNameEvan(arr,"Evan"));
+            int[] numbers = { 1, 40, 25, 76, 20, 3, 34 };
 
+
+            Console.WriteLine("1.) O(1) algorithm: " + isMyNameEvan("Evan"));
+            Console.WriteLine("2.) O(N) algorithm: " + isMyNameEvan(arr,"Evan"));
             Console.WriteLine("3.) O(N^2) algorithm: " + isMyNameEvan(arr, arr2));
             Console.WriteLine("4.) O(2^N) algorithm: " + Fibonacci(3));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Another example of O(N^2) is a Bubble Sort!");
+            Console.WriteLine("Original Array: ");
+            foreach (int n in numbers)
+                Console.Write(n + " ");
+            Console.WriteLine("\nSorted Array: ");
+            numbers = bubbleSort(numbers);
+            foreach (int n in numbers)
+                Console.Write(n + " ");
+            
+            Console.WriteLine("\n");
+
         }
 
         static bool isMyNameEvan(string input)
@@ -40,12 +53,12 @@ namespace BigONotationSample
 
         static bool isMyNameEvan(string[] namesList1, string[] namesList2)
         {
-            for (int i = 0; i < namesList1.Length; i++) //outer 
+            for (int count = 0; count < namesList1.Length; count++) //outer 
             {
-                for (int j = 0; j < namesList2.Length; j++) // inner
+                for (int innerCount = 0; innerCount < namesList2.Length; innerCount++) // inner
                 {
 
-                    if (namesList1[i] == namesList2[j])
+                    if (namesList1[count] == namesList2[innerCount])
                     {
                         return true;
                     }
@@ -64,6 +77,24 @@ namespace BigONotationSample
             return Fibonacci(number - 2) + Fibonacci(number - 1);
         }
 
-
+        static int[] bubbleSort(int[] arr)
+        {
+            int temp;
+            
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                for (int j = 0; j < arr.Length - 2; j++)
+                {
+                    if(arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            int[] newArr = arr;
+            return newArr;
+        }
     }
 }
